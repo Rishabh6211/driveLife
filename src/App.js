648e22bd851a89250ca5f443a253741a946313cc
Rishabh6211 <dsvvian.rishabh@gmail.com>
2023-07-24 20:21:49 +0530

    header implemented

--- src/App.js
+++ src/App.js
@@ -1,39 +1,80 @@
 
 import './App.css';
-import { Grid } from '@mui/material';
+import Header from './Header';
 
+const data = [
 
+]
 
 function App() {
   return (
-    <div>
-      <header  style={{flex:1}}>
+    <>
+   <Header />
+    {/* <header className='App-header'>
+      <div className='Left-header'>
+      <img alt="logo" className='App-logo' src='https://clipart-library.com/images/rcLonXkKi.png'  />
+      <strong className='LeftHeader-text'>DriveLife</strong>
+      </div>
+      <div className='Mid-header'>
+       <input className='App-search' type='text' placeholder='Search'/>
+       <button className='Search-button'>
+       <img className='Seach-icon' src={require('./assets/search-interface-symbol.png')}/>
+       </button>
+       
+      </div>
+      <div className='Right-header'>
+        <div>
+        <button className='App-sigin'>Signin</button>
+        </div>
+        <div>
+        <button className='App-sigUp'>SignUp</button>
+        </div>
+      </div>
      
-      <Grid container direction="row" className='App-header' md>
-     
-  <Grid xs={1} >
-  <img alt="logo" className='App-logo' src='https://clipart-library.com/images/rcLonXkKi.png'  />
-  </Grid>
-  <Grid xs={4}/>
-   
-   
-  <Grid xs={4} justifyContent={'center'} alignItems={'center'}>
-   <h1 className='App-text'>DriveLife</h1>
-  </Grid>
-  <Grid xs={1.5}  >
-   <button className='App-sigin'>Signin</button>
-  </Grid>
-  <Grid xs={1.5}  >
-   <button className='App-sigUp'>SignUp</button>
-  </Grid>
-</Grid>
-
-     
-      </header>
+    
       <div>
-        <button>hello</button>
+       
       </div>
+      </header> */}
+      {/* <div className='App-bodySection'>
+        <div className='grid1'> 
+            <img className='Grid-image' src={require('./assets/photo-1505740420928-5e560c06d30e.jpeg')}/>
+         </div>
+        <div className='grid2'> 2 </div>
+        <div className='grid3'> 3 </div>
+        <div className='grid1'> 4 </div>
+        <div className='grid2'> 5 </div>
+        <div className='grid3'> 6 </div>
+      </div> */}
+      <main className="main-content">
+      <div className="grid-container">
+      <a href="link_to_image_1_page" className="grid-item">
+          <img src={require('./assets/photo-1505740420928-5e560c06d30e.jpeg')} alt="Image 1" />
+          <p>Text for Image 1</p>
+        </a>
+        <a href="link_to_image_1_page" className="grid-item">
+          <img src={require('./assets/photo-1505740420928-5e560c06d30e.jpeg')} alt="Image 2" />
+          <p>Text for Image 2</p>
+          </a>
+        <a href="link_to_image_1_page" className="grid-item">
+          <img src={require('./assets/photo-1505740420928-5e560c06d30e.jpeg')} alt="Image 3" />
+          <p>Text for Image 3</p>
+          </a>
+        <a href="link_to_image_1_page" className="grid-item">
+          <img src={require('./assets/photo-1505740420928-5e560c06d30e.jpeg')} alt="Image 4" />
+          <p>Text for Image 4</p>
+          </a>
+        <a href="link_to_image_1_page" className="grid-item">
+          <img src={require('./assets/photo-1505740420928-5e560c06d30e.jpeg')}alt="Image 5" />
+          <p>Text for Image 5</p>
+          </a>
+        <a href="link_to_image_1_page" className="grid-item">
+          <img src={require('./assets/photo-1505740420928-5e560c06d30e.jpeg')} alt="Image 6" />
+          <p>Text for Image 6</p>
+          </a>
       </div>
+    </main>
+      </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Header.css'; // Create a new CSS file for header styles
+
+const Header = () => {
+  return (
+    <header className="header">
+      {/* Left Section */}
+      <div className="logo-section">
+        {/* Add your logo image here */}
+        <img src="https://clipart-library.com/images/rcLonXkKi.png" alt="Logo" className="logo" />
+        <span className="app-name">DriveLife</span>
+      </div>
+
+      {/* Middle Section */}
+      <div className="search-section">
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button>Search</button>
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="auth-section">
+        <button className="sign-in-button">Sign In</button>
+        <button className="sign-up-button">Sign Up</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
